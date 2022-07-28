46ef32f1fd0f1f216316cef0a29b0fc7c2feca08
markianholovchak <markiangolovchak@gmail.com>
2022-07-28 18:03:58 +0200

    Add login functionality

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -2,25 +2,27 @@ import { useState } from "react";
 import useAuthContext from "./useAuthContext";
 
 import axios from "axios";
-export default function useSignup() {
+
+export default function useLogin() {
 	const [error, setError] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
 	const { dispatch } = useAuthContext();
 
-	const signup = async (email, password) => {
+	const login = async (email, password) => {
 		setError(null);
 		setIsLoading(true);
-		const response = await axios.post("/api/user/signup", { email, password });
+		const response = await axios.post("/api/user/login", { email, password });
 		if (response.status !== 200) {
 			setIsLoading(false);
 		}
 		if (response.status === 200) {
 			// Save user to local storage
 			localStorage.setItem("user", JSON.stringify(response.data));
+
 			dispatch({ type: "LOGIN", payload: response.data });
 
 			setIsLoading(false);
 		}
 	};
-	return { signup, isLoading, error };
+	return { login, isLoading, error };
 }
